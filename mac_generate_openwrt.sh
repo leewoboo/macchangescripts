2fce688594245f7e63fcce0a554a3c3b3f51a966
Lee Woboo <leewoboo@riseup.net>
2015-10-19 20:29:48 +0000

    fix line that generates the MAC, partly deleted by mistake

--- mac_generate_openwrt.sh
+++ mac_generate_openwrt.sh
@@ -3,5 +3,5 @@
 # change here the path to maclist.lst
 PATH_MACLIST=maclist.lst
 
-MAC="$(head -$(( 1$(head -100 /dev/urandom | tr -dc "0123456789" | head -c5) % `wc -l < $PATH_MACLIST` + 1)) $PATH_MACLIST | tail -1 | awk '{print $1}'):$(head
+MAC="$(head -$(( 1$(head -100 /dev/urandom | tr -dc "0123456789" | head -c5) % `wc -l < $PATH_MACLIST` + 1)) $PATH_MACLIST | tail -1 | awk '{print $1}'):$(head -c 3 /dev/urandom |hexdump|head -1|sed -re 's/.{7} (..)(..) (..).+/\1:\2:\3/')"
 echo $MAC
